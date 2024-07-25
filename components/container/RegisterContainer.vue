3934ba3e16d8dd36bd170d0fba033a1a7a9eb899
LEEJINYOUN <swlsdud1005@naver.com>
2024-07-25 15:37:40 +0900

    회원가입 정규식 수정

--- components/container/RegisterContainer.vue
+++ components/container/RegisterContainer.vue
@@ -30,7 +30,7 @@ const regexCheck = (type: string, input: string) => {
   if (type == "email") {
     reg = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/i;
   } else if ((type = "name")) {
-    reg = /^[ㄱ-ㅎ가-힣a-zA-Z]+$/;
+    reg = /^[ㄱ-ㅎ가-힣a-zA-Z0-9]+$/;
   } else if ((type = "nickname")) {
     reg = /^[ㄱ-ㅎ가-힣a-zA-Z0-9]+$/;
   }
