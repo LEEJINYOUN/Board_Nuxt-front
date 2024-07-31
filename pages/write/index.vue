21d8547beb277d78662faf39aa715ac48b65eea8
LEEJINYOUN <swlsdud1005@naver.com>
2024-07-31 15:08:47 +0900

    이미지 업로드 수정

--- pages/write/index.vue
+++ pages/write/index.vue
@@ -88,8 +88,29 @@ const submit = async (e: any) => {
         />
       </div>
       <div class="mb-5">
-        <input type="file" @change="onChangeFile" />
-        <pre>{{ file?.name }}</pre>
+        <div class="border border-dashed border-gray-500 relative mb-3">
+          <input
+            type="file"
+            multiple
+            class="cursor-pointer relative block opacity-0 w-full h-full p-20 z-50"
+            @change="onChangeFile"
+          />
+          <div
+            class="text-center p-10 absolute top-0 right-0 left-0 m-auto text-gray-500"
+          >
+            <span>
+              <h4>
+                업로드 할 파일을
+                <br />드래그 하거나
+              </h4>
+              <p class="">선택하세요</p>
+            </span>
+          </div>
+        </div>
+        <div v-if="file" class="flex gap-4">
+          <h1>선택한 파일 :</h1>
+          <pre>{{ file?.name }}</pre>
+        </div>
       </div>
       <LazyButtonBlueButton type="submit" title="작성하기" />
     </form>
